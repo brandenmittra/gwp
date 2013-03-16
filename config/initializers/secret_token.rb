32e6142483c881ad8c338e7a043e54c8afb9bcd4
branden mittra <branden@univrs.co.uk>
2013-03-16 17:17:20 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gwp::Application.config.secret_token = '9dc441dfbaec8863cbcdc00ce96ff339de5833d10b49f00f441dbc2b28d8d4f881b378a27c455b8cc9a22f655c4edbfc01889aa84f8eee5e234cee0f358f7db0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gwp::Application.config.session_store :cookie_store, key: '_gwp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gwp::Application.config.session_store :active_record_store
