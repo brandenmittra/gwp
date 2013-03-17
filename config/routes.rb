d384ab87b67e69649f9b4ec4c567fd17742d34c1
branden mittra <branden@univrs.co.uk>
2013-03-17 18:41:08 +0000

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Gwp::Application.routes.draw do
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
 
-  root :to => 'pages#home'
+  root :to => 'pins#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
